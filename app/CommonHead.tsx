c89a5b94ff5abe6d414fdf028af01c5401598014
victor-aunon <hola@victoraunon.com>
2022-12-01 13:53:40 +0100

    DefaultHead replaced with CommonHead to avoid meta duplicity

---
+++ app/CommonHead.tsx
@@ -0,0 +1,9 @@
+export default function CommonHead(): JSX.Element {
+  return (
+    <>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="X-UA-Compatible, refresh" content="IE=edge" />
+    </>
+  )
+}
--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,22 @@
-import DefaultHead from './DefaultHead'
+import nextConfig from 'next.config'
+import { SiteConfig } from 'interfaces/SiteConfig'
+import CommonHead from './CommonHead'
 
 export default function Head(): JSX.Element {
+  const { name, website, siteImage } = nextConfig.siteConfig as SiteConfig
   return (
     <>
-      <DefaultHead />
+      <CommonHead />
+      <title>{name}</title>
+      <link rel="canonical" href={`https://${website}`} />
+      <meta name="description" content="" />
+      <meta name="robots" content="index, follow" />
+      {/* OPEN GRAPH */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={`https://${website}`} />
+      <meta property="og:title" content={name} />
+      <meta property="og:image" content={siteImage} />
+      <meta property="og:locale" content="es_ES" />
     </>
   )
 }
